38472a55e1099ab0bbad0ed7c20a06ad5260131b
Stanly Samuel <stanlyjohnsamuel@yahoo.com>
2022-08-31 16:25:24 +0530

    Created templated abstract visitor for VAST

--- visitors_ast/AbstractVASTVisitor.h
+++ visitors_ast/AbstractVASTVisitor.h
@@ -13,12 +13,42 @@ using namespace vast;
  * This class defines an interface, AbstractVASTVisitor for the VAST structure
  */
 namespace vastvisitor {
-    class AbstractVASTVisitor
+    class AbstractVASTVisitor<Result, Arg>
         {
             public:
-                virtual std::any visit(VAST* node){};
-                virtual std::any visit(VStatementExpr* node){};
-                virtual std::any visit(VID* node){};
+                virtual Result visit(VAST* node, Arg arg) = 0;
+                virtual Result visit(VStatementExpr* node, Arg arg) = 0;
+                virtual Result visit(VID* node, Arg arg) = 0;
+                virtual Result visit(VType* node, Arg arg) = 0;
+                virtual Result visit(VVarDecl* node, Arg arg) = 0;
+                virtual Result visit(VImport* node, Arg arg) = 0;
+                virtual Result visit(VImportList* node, Arg arg) = 0;
+                virtual Result visit(VVarDeclList* node, Arg arg) = 0;
+                virtual Result visit(VBehavioralSpec* node, Arg arg) = 0;
+                virtual Result visit(VTestSpec* node, Arg arg) = 0;
+                virtual Result visit(VTempSpec* node, Arg arg) = 0;
+                virtual Result visit(VInvSpec* node, Arg arg) = 0;
+                virtual Result visit(VSynthSpec* node, Arg arg) = 0;
+                virtual Result visit(VBinOp* node, Arg arg) = 0;
+                virtual Result visit(VBinStatementExpr* node, Arg arg) = 0;
+                virtual Result visit(VUnOp* node, Arg arg) = 0;
+                virtual Result visit(VUnStatementExpr* node, Arg arg) = 0;
+                virtual Result visit(VConstraintExpr* node, Arg arg) = 0;
+                virtual Result visit(VArgList* node, Arg arg) = 0;
+                virtual Result visit(VFunctionID* node, Arg arg) = 0;
+                virtual Result visit(VExecutedStatement* node, Arg arg) = 0;
+                virtual Result visit(VFinishedStatement* node, Arg arg) = 0;
+                virtual Result visit(VStartedStatement* node, Arg arg) = 0;
+                virtual Result visit(VRevertedStatement* node, Arg arg) = 0;
+                virtual Result visit(VWillSucceedStatement* node, Arg arg) = 0;
+                virtual Result visit(VBinExpr* node, Arg arg) = 0;
+                virtual Result visit(VUnExpr* node, Arg arg) = 0;
+                virtual Result visit(VVarExpr* node, Arg arg) = 0;
+                virtual Result visit(VConstExpr* node, Arg arg) = 0;
+                virtual Result visit(VFieldAccessExpr* node, Arg arg) = 0;
+                virtual Result visit(VArrAccessExpr* node, Arg arg) = 0;
+                virtual Result visit(VFuncCallExpr* node, Arg arg) = 0;
+                virtual Result visit(VFSumExpr* node, Arg arg) = 0;
         };
 }
 
